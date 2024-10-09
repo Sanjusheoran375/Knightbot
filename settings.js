7f2093edd6ce3ac6fd490a20b2d80a013b18decb
Sanjusheoran375 <sanjusheoran375@gmail.com>
2024-10-09 19:11:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'My Sticker Pack',
-  author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'SANJU',
+  author: 'SANJU',
+  botName: "MENTAL BOT",
+  botOwner: 'SANJU', // Your name
+  ownerNumber: '919728714060', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
